ad7b041c71660910412046b402a03f922664e98c
acsmacedo <andy.carlos.souza@hotmail.com>
2020-08-31 07:00:23 -0300

    Configuração incial

---
+++ src/frame/js/frame.js
@@ -0,0 +1,9 @@
+export { tab } from './body/tab.js';
+export { accordion } from './body/accordion.js';
+export { carousel } from './body/carousel.js';
+export { scrollUp } from './body/scroll-up.js';
+export { scrollDown } from './body/scroll-down.js';
+export { scrollProgress } from './body/scroll-progress.js';
+export { menuColapseSidebar } from './body/menu-colapse-sidebar.js';
+export { tooltips } from './body/tooltips.js';
+export { modal, openModal } from './body/modal.js';
---
+++ src/frame/js/script.js
@@ -0,0 +1,8 @@
+// importação obrigatória para o web-pack
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+// importação dos componentes do frame
+
+
+// My scripts
